6af96c504aaaa2745066aebd31850311c4b134f0
Raiyan <raiyankhalil@gmail.com>
2021-01-17 18:07:46 +0600

    testpy

--- face_detect.py
+++ face_detect.py
@@ -1,7 +1,7 @@
 import cv2
+import numpy as np 
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
 # img = cv2.imread('test.jpg')
 cap = cv2.VideoCapture(0)
